82895aea779c7d8d3578116c58f89ee46e8d4e7a
Jakub Tlach <tlach.jakub@gmail.com>
2018-02-22 19:08:35 +0900

    Web view v.1

--- iOS-Web-View/iOS-Web-View/ViewController.swift
+++ iOS-Web-View/iOS-Web-View/ViewController.swift
@@ -7,32 +7,64 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController, UIWebViewDelegate {
 
+
+	@IBOutlet weak var webView: WKWebView!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		let myWebView:UIWebView = UIWebView(frame: CGRect(x: 0, y: 20, width: self.view.frame.width, height: self.view.frame.height))
-		myWebView.delegate = self
-		self.view.addSubview(myWebView)
-		let url = URL (string: "https://r-up.jp/en/company/");
-		let request = URLRequest(url: url! as URL);
-		myWebView.loadRequest(request);
+		self.navigationController?.isNavigationBarHidden = true
+		
+		webView.allowsBackForwardNavigationGestures = true
+		
+		
+		// Adding webView content
+		do {
+			guard let filePath = Bundle.main.path(forResource: "http/index", ofType: "htm")
+				else {
+					// File Error
+					print ("File reading error")
+					return
+			}
+			
+			let contents =  try String(contentsOfFile: filePath, encoding: .utf8)
+			let baseUrl = URL(fileURLWithPath: filePath)
+			webView.loadHTMLString(contents as String, baseURL: baseUrl)
+		}
+		catch {
+			print ("File HTML error")
+		}
 	}
-
+	
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		// Hide the navigation bar on the this view controller
+		self.navigationController?.setNavigationBarHidden(true, animated: animated)
+	}
+	
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
 
+	
 	func webViewDidStartLoad(_ webView: UIWebView) {
 		print("web view start loading")
 	}
 	
+	
 	func webViewDidFinishLoad(_ webView: UIWebView) {
 		print("web view load completely")
 	}
+	
 	
 	func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
 		print("web view loading fail : ",error.localizedDescription)
